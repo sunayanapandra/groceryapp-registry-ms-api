82734b94f94d89c1a5cb96b698b925c784a83f02
sunayanapandra <pandrasunayana@gmail.com>
2022-05-05 17:09:25 +0530

    modified src

--- src/main/java/com/groceryapp/GroceryappRegistryMsApiApplication.java
+++ src/main/java/com/groceryapp/GroceryappRegistryMsApiApplication.java
@@ -8,6 +8,7 @@ public class GroceryappRegistryMsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryappRegistryMsApiApplication.class, args);
+		System.out.println(" ms working....");
 	}
 
 }
