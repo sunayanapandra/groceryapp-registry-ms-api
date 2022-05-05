2602ac6481fd70c146a4b13fad11cc975631d89e
sunayanapandra <pandrasunayana@gmail.com>
2022-05-05 16:47:52 +0530

    registry

---
+++ src/main/java/com/groceryapp/GroceryappRegistryMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.groceryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryappRegistryMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryappRegistryMsApiApplication.class, args);
+	}
+
+}
